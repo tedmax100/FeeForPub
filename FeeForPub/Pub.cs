31e067f94a92df7b0da21bfc96c9681a19868070
nathan <tedmax100@gmail.com>
2021-05-22 00:16:02 +0800

    [Feat] add pub class

---
+++ FeeForPub/Pub.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace FeeForPub
+{
+    public interface ICheckInFee
+    {
+        decimal GetFee(Customer customer);
+    }
+
+    public class Customer
+    {
+        public bool IsMale { get; set; }
+
+        public int Seq { get; set; }
+    }
+
+    public class Pub
+    {
+        private readonly ICheckInFee _checkInFee;
+        private decimal _inCome = 0;
+
+        public Pub(ICheckInFee checkInFee)
+        {
+            this._checkInFee = checkInFee;
+        }
+
+        /// <summary>
+        /// 入場
+        /// </summary>
+        /// <param name="customers"></param>
+        /// <returns>收費的人數</returns>
+        public int CheckIn(List<Customer> customers)
+        {
+            var result = 0;
+
+            foreach (var customer in customers)
+            {
+                var isFemale = !customer.IsMale;
+
+                //女生免費入場
+                if (isFemale)
+                {
+                    continue;
+                }
+                else
+                {
+                    //for stub, validate status: income value
+                    this._inCome += this._checkInFee.GetFee(customer);
+
+                    result++;
+                }
+            }
+
+            //for stub, validate return value
+            return result;
+        }
+
+        public decimal GetInCome()
+        {
+            return this._inCome;
+        }
+    }
+}
---
+++ FeeForPub/README.md
@@ -0,0 +1,11 @@
+﻿Calculation Fee for enter Pub
+===
+
+### 需求說明如下
+```
+當顧客進場時，
+如果是女生，則免費入場。
+若為男生，則根據 ICheckInFee 介面來取得門票的費用，
+並累計到 inCome 中。
+透過 GetInCome() 方法取得這一次的門票收入總金額
+```
