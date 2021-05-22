f1c2fce66c9f2f7cb4460774ff755e1391dc5009
nathan <tedmax100@gmail.com>
2021-05-22 16:09:06 +0800

    [UT] Add unit test

--- TestProject1/FeeForPubTest.cs
+++ TestProject1/FeeForPubTest.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 using FeeForPub;
+using FluentAssertions;
+using NSubstitute;
+
 namespace TestProject1
 {
     public class Tests
@@ -10,9 +14,51 @@ namespace TestProject1
         }
 
         [Test]
-        public void Test1()
+        public void Test_AmountOfCustomerThatHaveToPay()
         {
-            Assert.Pass();
+            // arrange
+            var customers = new List<Customer>
+            {
+                new Customer(){IsMale = true},
+                new Customer(){IsMale = true},
+                new Customer(){IsMale = true},
+                new Customer(){IsMale = false},
+                new Customer(){IsMale = false}
+            };
+            int expected = 3;
+
+            var stubCheckingFee = NSubstitute.Substitute.For<ICheckInFee>();
+            stubCheckingFee.GetFee(Arg.Any<Customer>()).Returns(100);
+
+            var pub = new Pub(stubCheckingFee);
+            // act
+            var customerThatHaveToPay = pub.CheckIn(customers);
+            // assert
+            customerThatHaveToPay.Should().Be(expected);
+        }
+        
+        [Test]
+        public void Test_Income()
+        {
+            // arrange
+            var customers = new List<Customer>
+            {
+                new Customer(){IsMale = true},
+                new Customer(){IsMale = true},
+                new Customer(){IsMale = true},
+                new Customer(){IsMale = false},
+                new Customer(){IsMale = false}
+            };
+            decimal expected = 300;
+            var stubCheckingFee = NSubstitute.Substitute.For<ICheckInFee>();
+            stubCheckingFee.GetFee( Arg.Any<Customer>()).Returns(100);
+
+            var pub = new Pub(stubCheckingFee);
+            // act
+            pub.CheckIn(customers);
+            var totalPay = pub.GetInCome();
+            // assert
+            totalPay.Should().Be(expected);
         }
     }
 }
